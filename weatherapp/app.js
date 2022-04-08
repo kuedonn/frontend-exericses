470e840fc7ad252aec079e268a7f1f476c987e2e
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-04-08 11:05:01 +0300

    weather app wannabe

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -1,36 +1,25 @@
-let lon;
-let lat;
-let temperature = document.querySelector('.temperature');
-let location = document.querySelector('.location');
-let summary = document.querySelector('.summary');
-let icon = document.querySelector('.icon');
-const kelvin = 273;
 
-window.addEventListener("load", ()=>{
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition((position)=>{
-            console.log(position);
-            lon = position.coords.longitude;
-            lat = position.coords.latitude;
+let button = document.querySelector('.button');
+let inputValue = document.querySelector('.inputValue');
+let cityname = document.querySelector('.cityname');
+let desc = document.querySelector('.desc');
+let temp = document.querySelector('.temp');
 
+button.addEventListener('click', function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=3915b57a37556d0743125578a4b6aaa8&units=metric")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
 
-const api_key = "3915b57a37556d0743125578a4b6aaa8";
- 
-//corinth lon 22.932030 lat 37.938580
-const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
-
-fetch(base).then((response)=>{
-    return response.json();
+    let nameValue = data['name'];
+    let tempValue = data['main']['temp'];
+    let descValue = data['weather'][0]['description']; 
+    cityname.innerHTML = nameValue;
+    temp.innerHTML = tempValue;
+    desc.innerHTML = descValue;
 })
-.then((data) => {
-    console.log(data);
-    temperature.textContent = Math.floor(data.main.temp - kelvin) + '°C';
-    summary.textContent = data.weather[0].descritpion;
-    loc.textContent = data.name + ',' + data.sys.country;
-    let icon1 = data.weather[0].icon;
-    icon.innerHTML = `<img scr="icons/${icon1}.svg" style='height:10rem'/>`;
-    
-    });
-   });    
-  }
-});
+.catch(err => alert("Wrong city name"))
+})
+
+
+
